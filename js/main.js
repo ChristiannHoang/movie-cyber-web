b4b4a550400fef01c4875e3601b7a43ab1c8b8c6
ChristiannHoang <hnhan793@gmail.com>
2023-04-19 02:00:54 +0700

    done new in

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.querySelector('header').style.top = '20px';
+//         document.querySelector('header').style.background = '#000';
+//     } else {
+//         document.querySelector('header').style.top = '-120px';
+//     }
+// }
