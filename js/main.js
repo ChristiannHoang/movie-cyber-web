21064d900a2290d0c0544f2d6a760b52ac6154af
Hoang Nhan <hnhan793@gmail.com>
2023-04-21 23:41:21 +0700

    fix bug header fixed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.querySelector('.header__fixed').style.top = '0';        
+        document.querySelector('.header__fixed').style.top = '0';     
+        document.querySelector('.header__fixed').style.display = 'block';
     } else {
         document.querySelector('.header__fixed').style.top = '-120px';
+        document.querySelector('.header__fixed').style.display = 'none';
     }
-    // console.log(document.body.scrollTop);
 }
 window.onscroll = function() {scrollFunction()};
 
