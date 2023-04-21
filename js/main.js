9eae5689b097e786baf4a443089a41362b4efda0
Hoang Nhan <hnhan793@gmail.com>
2023-04-22 00:03:02 +0700

    done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.querySelector('.header__fixed').style.top = '0';     
-        document.querySelector('.header__fixed').style.display = 'block';
+        document.querySelector('.header__fixed').style.top = '0';         
     } else {
-        document.querySelector('.header__fixed').style.top = '-120px';
-        document.querySelector('.header__fixed').style.display = 'none';
+        document.querySelector('.header__fixed').style.top = '-120px';    
     }
 }
 window.onscroll = function() {scrollFunction()};
